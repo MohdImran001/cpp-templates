8bdcb4da25f495c7b1b9658e453964257ec20c16
Mohd Imran <42844688+MohdImran001@users.noreply.github.com>
2020-11-10 20:43:40 +0530

    Create CodeChef.cpp

---
+++ CodeChef.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define FAST_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+using namespace std;
+
+const ll MAX = 1e5 + 1;
+
+int main()
+{
+    FAST_IO
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+      //  code
+    }
+    return 0;
+}
